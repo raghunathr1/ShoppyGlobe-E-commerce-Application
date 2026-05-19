f73cfe611cc67330ba1413205ccc7814ed814778
raghunathr1 <raghunthkapase2004@gmail.com>
2026-05-19 16:28:34 +0530

    Add Commit in Cart items  Section

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -4,6 +4,8 @@ import {removeFromCart, increaseQuantity, decreaseQuantity} from "../redux/cartS
 function CartItem({ item }) {
   const dispatch = useDispatch();
   return (
+    // Cart Items Details
+    
     <div className="cart-items">
     <div className="cart-item">
       <img src={item.thumbnail} alt={item.title} loading="lazy" width="150" />
@@ -11,6 +13,8 @@ function CartItem({ item }) {
         <h2>{item.title}</h2>
         <h3>Rs {item.price}</h3>
         <p> Quantity : {item.quantity}</p>
+
+        {/* Add Quantity Minus Quantity or Remove Item  */}
 
         <div>
           <button onClick={() => dispatch( decreaseQuantity(item.id))} > - </button>
